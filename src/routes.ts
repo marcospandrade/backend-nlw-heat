6cc638d4ed6be2976bb3dc6dc06fa44576bf7db4
Marcos Andrade <marcos.andrade@pitang.com>
2021-10-22 01:44:32 -0300

    get last 3 messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticatedUserController } from "./controllers/AuthenticatedUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -11,5 +12,6 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+router.get("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
